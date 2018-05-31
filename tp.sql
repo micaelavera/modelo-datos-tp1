17d4170917320bf17db3d7d95d7439ecc733a48e
Liz Katheryna Moreno López <moreno.lizk@gmail.com>
2018-05-31 22:06:39 +0000

    agrego tabla

--- tp.sql
+++ tp.sql
@@ -1,4 +1,3 @@
-
 drop database if exists tarjeta;
 
 create database tarjeta;
@@ -15,4 +14,4 @@ alter table cliente add constraint cliente_pk primary key (dni);
 alter table tarjeta add constraint direccion_pk primary key (codigoSeguridad);
 alter table comercio add constraint compra_pk primary key (codigo_postal);
 
-alter table compra add constraint compra_fk1 foreign key (numero) references bote(tarjeta);
+alter table compra add constraint compra_fk1 foreign key (numero_tarjeta) references tarjeta(numero);
