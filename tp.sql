1f34bf747fbb112d250c083fe9e0961278d82960
Micaela <micavera2010@hotmail.com>
2018-05-31 21:43:40 -0300

    add tablas y comentarios

--- tp.sql
+++ tp.sql
@@ -1,5 +1,4 @@
 drop database if exists tarjeta;
-
 create database tarjeta;
 
 create table cliente(dni integer,
@@ -33,9 +32,10 @@ create table factura(codigo_factura integer,
 					 total_pagar real,
 					 detalle_articulo integer);
 --detalle es la fk de factura 
+-- ver si compra contiene a factura o viceversa.
 
 create table detalle(codigo_articulo integer,
-			cantidad integer,
+   			cantidad integer,
 			precio_unitario real);
 
 create table resumen(codigo_factura integer,
@@ -44,9 +44,10 @@ create table resumen(codigo_factura integer,
 			 total_pagar real,
 		 	 numero_tarjeta integer);		
 
-
 alter table cliente add constraint cliente_pk primary key (dni);
 alter table tarjeta add constraint tarjeta_pk primary key (numero);
 alter table comercio add constraint compra_pk primary key (cod_comercio);
 alter table compra add constraint compra_fk1 foreign key (numero_tarjeta) references tarjeta(numero);
 alter table compra add constraint compra_fk2 foreign key (cod_comercio) references comercio(cod_comercio);
+
+--agregar pks y fks que faltan y arreglar nombre que se "cambiaron"
