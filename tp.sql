73f9ea2950bd87e8f6245c048c138be3a3df8b18
Micaela <micavera2010@hotmail.com>
2018-06-02 16:44:13 -0300

    fix identacion de las tablas

--- tp.sql
+++ tp.sql
@@ -1,48 +1,64 @@
 drop database if exists tarjeta;
 create database tarjeta;
 
-create table cliente(dni integer,
-			 nombre varchar(64),
-			 apellido varchar(64),
-			 direccion varchar(64),
-			 telefono integer);
+\c tarjeta
 
-create table tarjeta(codigo_tarjeta integer,
-			 codigo_seguridad integer, 
-			 dni_cliente integer,
-			 limite_compra integer,
-			 saldo real,
-			 fecha_vencimiento date,
-			 estado_tarjeta varchar(64));
+create table cliente(
+	    dni integer,
+		nombre varchar(64),
+		apellido varchar(64),
+		direccion varchar(64),
+		telefono integer
+);
+
+create table tarjeta(
+	    numero_tarjeta integer,
+		dni_cliente integer,
+		codigo_seguridad integer, 
+		limite_compra integer,
+		saldo real,
+		fecha_vencimiento date,
+		estado_tarjeta varchar(64)
+);
 		/* estado_tarjeta comprende el estado de vigencia,suspendida,etc, se podria poner un enum */
 
-create table comercio(codigo_comercio integer,
-			 nombre varchar(64),
-			 direccion varchar(64),
-			 codigo_postal integer);
+create table comercio(
+	    codigo_comercio integer,
+	    nombre varchar(64),
+		direccion varchar(64),
+		codigo_postal integer
+);
 
-create table compra(codigo_compra integer, 
-             numero_tarjeta integer,
-			 fecha date,
-			 horario time);
+create table compra(
+	    codigo_compra integer, 
+        numero_tarjeta integer,
+		fecha date,
+	    horario time
+);
 
-create table factura(codigo_factura integer,
-					 dni_cliente integer,
-					 codigo_comercio integer,
-					 total_pagar real,
-					 detalle_articulo integer);
+create table factura(
+	    codigo_factura integer,
+		dni_cliente integer,
+	    codigo_comercio integer,
+		total_pagar real,
+		detalle_articulo integer
+);
 --detalle es la fk de factura 
 -- ver si compra contiene a factura o viceversa.
 
-create table detalle(codigo_articulo integer,
-   			cantidad integer,
-			precio_unitario real);
+create table detalle(
+	    codigo_articulo integer,
+   		cantidad integer,
+		precio_unitario real
+);
 
-create table resumen(codigo_factura integer,
-			 periodo_facturado integer,
-		     fecha_vencimiento date,
-			 total_pagar real,
-		 	 numero_tarjeta integer);		
+create table resumen(
+	    codigo_factura integer,
+		periodo_facturado integer,
+	    fecha_vencimiento date,
+		total_pagar real,
+		numero_tarjeta integer
+);		
 
 alter table cliente add constraint cliente_pk primary key (dni);
 alter table tarjeta add constraint tarjeta_pk primary key (numero);
