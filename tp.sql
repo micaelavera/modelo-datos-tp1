57036373004e6ebb71657f5919a1255fb145218e
Liz Katheryna Moreno López <moreno.lizk@gmail.com>
2018-06-03 23:53:07 +0000

    Update tp.sql

--- tp.sql
+++ tp.sql
@@ -60,18 +60,19 @@ create table resumen(
 		numero_tarjeta integer
 );		
 --PRIMARY KEY--
-alter table cliente add constraint cliente_pk primary key (dni);
-alter table tarjeta add constraint tarjeta_pk primary key (numero_tarjeta);
+alter table cliente  add constraint cliente_pk  primary key (dni);
+alter table tarjeta  add constraint tarjeta_pk  primary key (numero_tarjeta);
 alter table comercio add constraint comercio_pk primary key (codigo_comercio);
-alter table compra add constraint compra_pk primary key(codigo_compra);
-alter table factura add constraint factura_pk primary key (codigo_factura);
-alter table detalle add constraint detalle_pk primery key (codigo_articulo);
+alter table compra   add constraint compra_pk   primary key(codigo_compra);
+alter table factura  add constraint factura_pk  primary key (codigo_factura);
+alter table detalle  add constraint detalle_pk  primary key (codigo_articulo);
 
 --FOREIGN KEY--
-alter table tarjeta add constraint tarjeta_fk0 foreign key (dni_cliente) references cliente(dni);
-alter table compra add constraint compra_fk0 foreign key (numero_tarjeta) references tarjeta(numero_tarjeta);
-alter table factura add constraint factura_fk0 foreign key (dni_cliente) references cliente (dni);
+alter table tarjeta add constraint tarjeta_fk0 foreign key (dni_cliente)      references cliente(dni);
+alter table compra  add constraint compra_fk0  foreign key (numero_tarjeta)   references tarjeta(numero_tarjeta);
+alter table factura add constraint factura_fk0 foreign key (dni_cliente)      references cliente (dni);
 alter table factura add constraint factura_fk1 foreign key (detalle_articulo) references detalle(codigo_articulo);
-alter table resumen add constraint detalle_fk0 foreign key (codigo_factura) references factura(codigo_factura);
+alter table resumen add constraint detalle_fk0 foreign key (codigo_factura)   references factura(codigo_factura);
+alter table resumen add constraint detalle_fk1 foreign key (numero_tarjeta)   references tarjeta(numero_tarjeta);
 
 --agregar pks y fks que faltan y arreglar nombres que se "cambiaron"
